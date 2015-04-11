8e3187c1843eeac44459a5a8aad257e8cb7566f2
Pär Strandberg <par.strandberg@gmail.com>
2015-04-11 21:40:07 +0200

    More files. Should be excluded by gitignore.. but..

---
+++ unity/Dwarven Conflicts/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    private Animator anim;              // Reference to the animator component.
+    private float moveSpeed = 3f;
+
+    // Use this for initialization
+	void Start () {
+       //
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate () {
+        // Cache the inputs.
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        movement(h, v);
+    }
+
+    void movement (float h, float v) {
+        transform.Translate(h * moveSpeed * Time.deltaTime, v * moveSpeed * Time.deltaTime, 0);
+    }
+}
