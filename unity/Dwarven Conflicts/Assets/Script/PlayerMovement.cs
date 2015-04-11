22283d58ed9078f7676b265f2130e934b622a25f
Pär Strandberg <par.strandberg@gmail.com>
2015-04-12 01:03:07 +0200

    Start of basic state machine added

--- unity/Dwarven Conflicts/Assets/Script/PlayerMovement.cs
+++ unity/Dwarven Conflicts/Assets/Script/PlayerMovement.cs
@@ -3,22 +3,38 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
     private float moveSpeed = 3f;
+    private Animator anim;
+    int jumpHash = Animator.StringToHash("jump");
+    int walkHash = Animator.StringToHash("walk");
+    private AnimatorStateInfo currentBaseState;         // a reference to the current state of the animator, used for base layer
+
+
 
     // Use this for initialization
 	void Start () {
-       //
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        currentBaseState = anim.GetCurrentAnimatorStateInfo(0); // set our currentState variable to the current state of the Base Layer (0) of animation
 
-        movement(h, v);
-    }
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        bool j = Input.GetButtonDown("Jump");
 
-    void movement (float h, float v) {
-        float forwardMovement = h * moveSpeed * Time.deltaTime;        
+        movement(x, y, j);
+
+        print(currentBaseState.fullPathHash);
+        anim.SetFloat(walkHash, x);
+}
+
+    void movement (float x, float y, bool jump) {
+        float forwardMovement = x * moveSpeed * Time.deltaTime;        
         transform.Translate(forwardMovement, 0, 0);
+
+        if (jump) {
+            anim.SetTrigger(jumpHash);
+        }
     }
 }
