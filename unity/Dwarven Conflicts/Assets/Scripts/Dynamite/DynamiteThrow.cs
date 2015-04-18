8e16459d7261459efe0c7ac54f0421730d39f9cc
Pär Strandberg <par.strandberg@gmail.com>
2015-04-18 14:23:31 +0200

    Quick fix from last weird merge and preparation for dynamite

---
+++ unity/Dwarven Conflicts/Assets/Scripts/Dynamite/DynamiteThrow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamiteThrow : MonoBehaviour {
+    private Rigidbody2D rb;
+    public float speed = 18.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- unity/Dwarven Conflicts/Assets/Scripts/Player/Player.cs
+++ unity/Dwarven Conflicts/Assets/Scripts/Player/Player.cs
@@ -3,26 +3,34 @@ using System.Collections;
 
 [RequireComponent (typeof (PlayerController))]
 public class Player : MonoBehaviour {
-
+    //Variables
 	private float jumpHeight = 2;
 	private float timeToJumpApex = 0.3f;
 	float accelerationTimeAirborne = 0.2f;
 	float accelerationTimeGrounded = 0.1f;
 	float moveSpeed = 6f;
 
+    //Game variables
 	float gravity;
 	float jumpVelocity;
 	Vector3 velocity;
 	float velocityXSmoothing;
 
+    //Dynamite
+    public GameObject dynamite;
+    public Transform dynamiteSpawn;
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f;
+
 	PlayerController controller;
 
 	void Start() {
 		controller = GetComponent<PlayerController> ();
+        //dynamite = gameObject.GetComponent<Dynamite>();
+        dynamiteSpawn = GetComponent<Transform>();
 
 		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
 		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-		print ("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
 	}
 
 	void Update() {
@@ -36,6 +44,12 @@ public class Player : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
 			velocity.y = jumpVelocity;
 		}
+
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            //Instantiate(dynamite, dynamiteSpawn.position, dynamiteSpawn.rotation);
+        }
 
 		float targetVelocityX = input.x * moveSpeed;
 		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
