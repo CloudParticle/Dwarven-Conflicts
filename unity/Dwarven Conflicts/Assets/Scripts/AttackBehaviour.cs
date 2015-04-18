d3f9af5844a661006cde5056e5e957e221c313d1
Pär Strandberg <par.strandberg@gmail.com>
2015-04-18 09:15:33 +0100

    Minor restructuring of files/folders

---
+++ unity/Dwarven Conflicts/Assets/Scripts/AttackBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class AttackBehaviour : StateMachineBehaviour
+{
+    public GameObject particle;
+    public float radius;
+    public float power;
+
+    protected GameObject clone;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("On State Enter ");        
+    }
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("On Exit ");
+    }
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("On Attack Update ");
+    }
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("On Attack Move ");
+    }
+    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("On Attack IK ");
+    }
+}
